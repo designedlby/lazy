c8c0a16c26c587963f259a46e20ecb0ba9d77dbb
designedlby <92264212+designedlby@users.noreply.github.com>
2024-11-22 22:06:13 +0200

    Update ADS.js

--- ADS.js
+++ ADS.js
@@ -6,4 +6,4 @@ var collection = document.getElementsByClassName("GONOW");
     collection[i].onclick = function myFunction2() {window.open('https://youtu.be/MtjtjXY0bvg?si=5S_yxsiZ16Qtx-Cs','window1','height=600,width=600,screenX=100,screenY=100'); 
         window.open('https://hudhud-platform.blogspot.com/2024/11/noon-discount-code.html','window2','height=800,width=400,screenX=700,screenY=100');}
         }
-        }     
+        }; 
