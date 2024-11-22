0c6ab298a6513881a1403f37f1f5078f34fc9bdd
designedlby <92264212+designedlby@users.noreply.github.com>
2024-11-22 23:10:50 +0200

    Update ADDD.js

--- ADDD.js
+++ ADDD.js
@@ -1,21 +1,12 @@
 document.write("This text comes from an external script.");
-window.onload = function() {
-  // Get the collection of elements with class "GONOW"
-  var collection = document.getElementsByClassName("GONOW");
-
-  // Check if collection exists, otherwise length will be 0
+var collection = document.getElementsByClassName("GONOW");
   if (collection !== null) {
     var len = collection.length;
 
-    // Loop through each element in the collection
     for (var i = 0; i < len; i++) {
-      collection[i].onclick = function() {
-        // Open the YouTube video in window1
+      collection[i].onclick = 
         window.open('https://youtu.be/MtjtjXY0bvg?si=5S_yxsiZ16Qtx-Cs', 'window1', 'height=600,width=600,screenX=100,screenY=100');
 
-        // Open the blog post in window2
         window.open('https://hudhud-platform.blogspot.com/2024/11/noon-discount-code.html', 'window2', 'height=800,width=400,screenX=700,screenY=100');
       };
-    }
-  }
-};
+    };
