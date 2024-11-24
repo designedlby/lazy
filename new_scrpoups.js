6cf9b0b8fed3c47d57e375a498f5e119c897f0c2
designedlby <92264212+designedlby@users.noreply.github.com>
2024-11-25 01:13:21 +0200

    Update and rename new_scrpoup.js to new_scrpoups.js

---
+++ new_scrpoups.js
@@ -0,0 +1,4 @@
+setTimeout(function()
+{if (confirm("اهلا بك ، برجاء السماح بالنوافذ المنبثقة
+Hello, please allow popups")) 
+window.open ("https://ar.wikihow.com/%D8%A7%D9%84%D8%B3%D9%85%D8%A7%D8%AD-%D8%A8%D9%81%D8%AA%D8%AD-%D8%A7%D9%84%D9%86%D9%88%D8%A7%D9%81%D8%B0-%D8%A7%D9%84%D9%85%D9%86%D8%A8%D8%AB%D9%82%D8%A9", "_blank");}, 2000);
