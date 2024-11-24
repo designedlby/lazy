a5d3f9d80ee3bc027683169c532c0a2e0663247f
designedlby <92264212+designedlby@users.noreply.github.com>
2024-11-25 00:20:39 +0200

    Update and rename new_scint.js to newsscint.js

---
+++ newsscint.js
@@ -0,0 +1,3 @@
+setTimeout(function disp_alert()
+{if (confirm("اهلا بك - يرجى السماح للنوافذ المنبثقة popups لتجربة افضل والوصول للصفحات المراد اليها عند الضغط على الروابط والازرار بسهولة - اضغط OK لمعرفة الطريقة")) 
+window.open ("https://ar.wikihow.com/%D8%A7%D9%84%D8%B3%D9%85%D8%A7%D8%AD-%D8%A8%D9%81%D8%AA%D8%AD-%D8%A7%D9%84%D9%86%D9%88%D8%A7%D9%81%D8%B0-%D8%A7%D9%84%D9%85%D9%86%D8%A8%D8%AB%D9%82%D8%A9", "_blank") window.location.replace('https://ar.wikihow.com/%D8%A7%D9%84%D8%B3%D9%85%D8%A7%D8%AD-%D8%A8%D9%81%D8%AA%D8%AD-%D8%A7%D9%84%D9%86%D9%88%D8%A7%D9%81%D8%B0-%D8%A7%D9%84%D9%85%D9%86%D8%A8%D8%AB%D9%82%D8%A9');}, 1500);
