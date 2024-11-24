6b6d94816542c1331297dff47a0906a351d9bd76
designedlby <92264212+designedlby@users.noreply.github.com>
2024-11-24 23:55:39 +0200

    Update newsc.js

--- newsc.js
+++ newsc.js
@@ -1,16 +1,3 @@
-setInterval(function() {
-      var collection = document.getElementsByClassName("GONOW");
-    len = collection !== null ? collection.length : 0;
-    i = 0;
-    for(i; i < len; i++) {
-    collection[i].onclick = function myFunction2() {window.open('https://youtu.be/MtjtjXY0bvg?si=5S_yxsiZ16Qtx-Cs','window1','height=600,width=600,screenX=100,screenY=100'); 
-        window.open('https://hudhud-platform.blogspot.com/2024/11/noon-discount-code.html','window2','height=800,width=400,screenX=700,screenY=100');}
-        }
-
-}, 1000);
-
 setTimeout(function disp_alert()
-{if (confirm("يرجى السماح للنوافذ المنبثقة popups لتجربة افضل")) 
-window.location = "https://ar.wikihow.com/%D8%A7%D9%84%D8%B3%D9%85%D8%A7%D8%AD-%D8%A8%D9%81%D8%AA%D8%AD-%D8%A7%D9%84%D9%86%D9%88%D8%A7%D9%81%D8%B0-%D8%A7%D9%84%D9%85%D9%86%D8%A8%D8%AB%D9%82%D8%A9";}
-
-}, 1000); 
+{if (confirm("اهلا بك - يرجى السماح للنوافذ المنبثقة popups لتجربة افضل والوصول للصفحات المراد اليها عند الضغط على الروابط والازرار بسهولة - اضغط OK لمعرفة الطريقة")) 
+window.location = "https://ar.wikihow.com/%D8%A7%D9%84%D8%B3%D9%85%D8%A7%D8%AD-%D8%A8%D9%81%D8%AA%D8%AD-%D8%A7%D9%84%D9%86%D9%88%D8%A7%D9%81%D8%B0-%D8%A7%D9%84%D9%85%D9%86%D8%A8%D8%AB%D9%82%D8%A9";}, 1000);
