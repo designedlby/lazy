d4b972eaad6787bb84169a3b765bfd637fa0ec25
designedlby <92264212+designedlby@users.noreply.github.com>
2024-11-30 12:20:26 +0200

    Update and rename frameadds.js to frmadds.js

--- frmadds.js
+++ frmadds.js
@@ -2,6 +2,14 @@ setTimeout(function Firstfunc() {
 var divElement = document.createElement("Div");
 divElement.id = "divID";
 
+document.getElementsByTagName("body")[0].appendChild(divElement);
+    
+var frmElement = document.createElement("iframe");
+frmElement.id = "framead";
+frmElement.src = "https://html-preview.github.io/?url=https://raw.githubusercontent.com/designedlby/lazy/refs/heads/main/framad.html";
+
+divElement.appendChild(frmElement);
+    
 setTimeout(function() {
 var button = document.createElement("Button");
 var textForButton = document.createTextNode("close ads");
@@ -12,12 +20,4 @@ button.addEventListener("click", function(){
 });
 
 divElement.appendChild(button)}, 5000);
-
-var frmElement = document.createElement("iframe");
-frmElement.id = "framead";
-frmElement.src = "https://html-preview.github.io/?url=https://raw.githubusercontent.com/designedlby/lazy/refs/heads/main/framad.html";
-
-divElement.appendChild(frmElement);
-
-document.getElementsByTagName("body")[0].appendChild(divElement);
 }, 3000);
