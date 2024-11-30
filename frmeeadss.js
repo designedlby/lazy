8acc96af14e0bfccf703226e4dd3054888c72aee
designedlby <92264212+designedlby@users.noreply.github.com>
2024-11-30 13:04:24 +0200

    Update and rename frmeadss.js to frmeeadss.js

--- frmeeadss.js
+++ frmeeadss.js
@@ -15,8 +15,8 @@ var button = document.createElement("Button");
 var textForButton = document.createTextNode("close ads");
 button.appendChild(textForButton);
 button.addEventListener("click", function(){
-    divElement.style.display = "none";
-    frmElement.src = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg0wUGJOmhT_kU3f4un19sFiVa8ESU2YNW97zBNdfD_aqGK0nDga3aJRqTjcSoH8DqG0K4lRhPqo36W3YDyTCR_2Ui3eFgbRdB7IQgu008fctfp8IG6a-VNc0j5i0yBtJ12nL_Qqua2Oor1pcDfYicgMSUHTLaH1zNUvDGyKuAbwo4_2SG7grG-YYrDs3At/s16000/New%20Project%20(3).jpg";
+    divElement.style.opacity = "0";
+    frmElement.src = "";
 });
 
 divElement.appendChild(button)}, 5000);
