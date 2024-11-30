ef2c49f0860f2352b2d2357d270eff38d73ea2cb
designedlby <92264212+designedlby@users.noreply.github.com>
2024-11-30 21:58:08 +0200

    Update and rename frmas.js to frmdas.js

--- frmdas.js
+++ frmdas.js
@@ -3,13 +3,7 @@ var divElement = document.createElement("Div");
 divElement.id = "divID";
 
 document.getElementsByTagName("body")[0].appendChild(divElement);
-    
-var frmElement = document.createElement("iframe");
-frmElement.id = "framead";
-frmElement.src = "https://html-preview.github.io/?url=https://raw.githubusercontent.com/designedlby/lazy/refs/heads/main/framad.html";
 
-divElement.appendChild(frmElement);
-    
 var button = document.createElement("Button");
 button.id = "xallad";
 var textForButton = document.createTextNode("close ads");
@@ -17,6 +11,12 @@ button.appendChild(textForButton);
 button.addEventListener("click", function(){
     
 });
+
+var frmElement = document.createElement("iframe");
+frmElement.id = "framead";
+frmElement.src = "https://html-preview.github.io/?url=https://raw.githubusercontent.com/designedlby/lazy/refs/heads/main/framad.html";
+
+divElement.appendChild(frmElement);
 
 divElement.appendChild(button)};
 }, 3000);
