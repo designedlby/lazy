a33b16945c9e05e93e34e7627ed43b819db74a8d
designedlby <92264212+designedlby@users.noreply.github.com>
2024-12-01 03:19:47 +0200

    Update and rename frmsdaa.js to frmssdaa.js

--- frmssdaa.js
+++ frmssdaa.js
@@ -2,23 +2,22 @@ setTimeout(function Firstfunc() {
 var divElement = document.createElement("Div");
 divElement.id = "divID";
 
-
-var buttonn = document.createElement("Button");
-buttonn.id = "xallad";
+setTimeout(function() {
+var button = document.createElement("Button");
 var textForButton = document.createTextNode("close ads");
-buttonn.appendChild(textForButton);
-buttonn.addEventListener("click", function(){
+button.appendChild(textForButton);
+button.addEventListener("click", function(){
     divElement.style.display = "none";
-    frmElement.src = "";
+    frmElement.src = "#";
 });
 
-
-divElement.appendChild(buttonn);
+divElement.appendChild(button)}, 5000);
 
 var frmElement = document.createElement("iframe");
 frmElement.id = "framead";
 frmElement.src = "https://html-preview.github.io/?url=https://raw.githubusercontent.com/designedlby/lazy/refs/heads/main/framad.html";
 
 divElement.appendChild(frmElement);
+
 document.getElementsByTagName("body")[0].appendChild(divElement);
 }, 3000);
